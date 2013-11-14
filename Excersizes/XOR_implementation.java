0698d8be36359fb7959a5f43627952d959f03ac1
Taylor Blau <me@ttaylorr.com>
2013-11-14 08:18:58 -0500

    Add XOR implementation

---
+++ Excersizes/XOR_implementation.java
@@ -0,0 +1,18 @@
+public class XOR {
+
+	public static void main(String[] args) {
+	
+		boolean a;
+		boolean b;
+		
+		boolean result;
+		
+		System.out.println(result = doXOR(a,b));
+	
+	}
+
+	public boolean doXOR(boolean a, boolean b) {
+		return a != b;
+	}
+	
+}

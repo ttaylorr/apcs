04fd2cb99166ece9b9cfaec1c60a58061ff5d16d
Taylor Blau <me@ttaylorr.com>
2013-11-15 15:02:46 -0500

    Add reverse name thing (very fun).

---
+++ Name/Name.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Name {
+
+	public static void main(String[] args) {
+		List<String> reverse = Arrays.asList("Noah Momblanco".split(" "));
+		Collections.reverse(reverse);
+		String output = (Arrays.toString(reverse.toArray())).replace('[', ' ').replace(']', ' ').trim();
+		System.out.println(output);
+	}
+
+}

e45b8314769e04d46478d8682352c4bf82c6ea8f
nmombo12 <nmombo12@gmail.com>
2014-02-20 15:08:28 -0500

    Remove Real Name
    Removes a real-life name and replaces it with the name Sample Name.

--- Name/Name.java
+++ Name/Name.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class Name {
 
 	public static void main(String[] args) {
-		List<String> reverse = Arrays.asList("Noah Momblanco".split(" "));
+		List<String> reverse = Arrays.asList("Sample Name".split(" "));
 		Collections.reverse(reverse);
 		String output = (Arrays.toString(reverse.toArray())).replace('[', ' ').replace(']', ' ').trim();
 		System.out.println(output);
